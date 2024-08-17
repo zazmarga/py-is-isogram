754ba754825a179b8b8883903f8f4b0eb90676fb
Marharyta Zazulia <zazmargo@gmail.com>
2024-08-17 13:38:22 -0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,40 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+
+    @pytest.mark.parametrize(
+        "word,expected_answer",
+        [
+            ("", True),
+            (" ", True),
+            ("  ", False),
+            ("moon", False),
+            ("Ala", False),
+            ("kajh/ndvyw$5#", True),
+            ("playgrounds", True)
+        ]
+    )
+    def test_is_isogram_expected_answer(
+            self,
+            word: str,
+            expected_answer: bool
+    ) -> None:
+        assert is_isogram(word) == expected_answer
+
+    @pytest.mark.parametrize(
+        "word,expected_error",
+        [
+            (5, AttributeError),
+            ({1}, AttributeError)
+        ]
+    )
+    def test_raising_test_correctly(
+            self,
+            word: str,
+            expected_error: [Exception]
+    ) -> None:
+        with pytest.raises(expected_error):
+            is_isogram(word)
